d0390136b5c3ac7dfacd42329b65f3a68c630519
wufangmin <kupature@163.com>
2026-08-27 11:26:32 +0800

    使用http.server优雅启停

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"log/slog"
 	_ "minsky/go-template/docs"          // swagger WebUI访问需要
 	_ "minsky/go-template/internal/init" // 触发viper读取json配置
 	"minsky/go-template/internal/routers"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,11 +28,66 @@ import (
 // @license.name Apache 2.0
 // @host localhost:8000
 func main() {
+
+	appCtx, stopSignal := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+	)
+	defer stopSignal()
+
 	gin.ForceConsoleColor()
 	router := gin.Default()
 	routers.BindApi(router)
-	err := router.Run(":8000")
-	if err != nil {
-		return
+
+	server := &http.Server{
+		Addr:              ":8000",
+		Handler:           router,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      15 * time.Second,
+		IdleTimeout:       60 * time.Second,
 	}
+
+	serverErr := make(chan error, 1)
+	go func() {
+		slog.Info("Gin 服务启动", "url", "http://127.0.0.1"+server.Addr)
+		err := server.ListenAndServe()
+
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			serverErr <- err
+			return
+		}
+		serverErr <- nil
+	}()
+
+	select {
+	case <-appCtx.Done():
+		slog.Info("收到关闭信号:", context.Cause(appCtx))
+	case err := <-serverErr:
+		if err != nil {
+			slog.Info("HTTP 服务异常退出:", err)
+		} else {
+			slog.Info("HTTP 服务已经停止")
+		}
+	}
+
+	stopSignal()
+
+	slog.Info("HTTP服务开始关闭")
+	shutdownCtx, cancelShutdown := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancelShutdown()
+
+	// 停止接受新请求, 并等待正在处理的请求完成
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		slog.Error("HTTP服务关闭超时:%v", err)
+
+		if closeErr := server.Close(); closeErr != nil {
+			slog.Error("强制关闭HTTP服务失败: %v", closeErr)
+		}
+	} else {
+		slog.Info("HTTP服务已关闭")
+	}
+
 }
